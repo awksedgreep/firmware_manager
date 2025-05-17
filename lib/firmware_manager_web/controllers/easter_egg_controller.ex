1ef58961621c3bb1a651f9a9223be155334c3dca
Mark Cotner <mark.cotner@gmail.com>
2025-05-17 18:14:42 -0400

    refactor: simplify phoenix easter egg

--- lib/firmware_manager_web/controllers/easter_egg_controller.ex
+++ lib/firmware_manager_web/controllers/easter_egg_controller.ex
@@ -2,6 +2,7 @@ defmodule FirmwareManagerWeb.EasterEggController do
   use FirmwareManagerWeb, :controller
 
   def phoenix(conn, _params) do
+    # The app layout will be used automatically
     render(conn, :phoenix)
   end
 end
